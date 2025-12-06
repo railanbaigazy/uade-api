39d9f2651fe36789b20ee268bd1b5970c6b9a2b7
gunyawa <ahatova14.08@gmail.com>
2025-12-06 12:45:51 +0500

    feat(cronjobs): add sample reminders and cleanup jobs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,33 +1,39 @@
 package main
 
 import (
-	"fmt"
-	"log"
-	"net/http"
+    "fmt"
+    "log"
+    "net/http"
 
-	"github.com/jmoiron/sqlx"
-	_ "github.com/lib/pq"
+    "github.com/jmoiron/sqlx"
+    _ "github.com/lib/pq"
 
-	"github.com/railanbaigazy/uade-api/internal/app"
-	"github.com/railanbaigazy/uade-api/internal/config"
+    "github.com/railanbaigazy/uade-api/internal/app"
+    "github.com/railanbaigazy/uade-api/internal/config"
+    "github.com/railanbaigazy/uade-api/internal/cronjobs"
 )
 
 func main() {
-	cfg := config.Load()
+    cfg := config.Load()
 
-	db, err := sqlx.Open("postgres", cfg.DBURL)
-	if err != nil {
-		log.Fatal("Failed to connect to DB:", err)
-	}
-	defer db.Close()
+    // Подключение к базе (можно временно закомментировать, если нет DB)
+    db, err := sqlx.Open("postgres", cfg.DBURL)
+    if err != nil {
+        log.Println("Не удалось подключиться к БД, продолжаем без неё:", err)
+    } else {
+        defer db.Close()
+        if err := db.Ping(); err != nil {
+            log.Println("База недоступна, продолжаем без неё:", err)
+        }
+    }
 
-	if err := db.Ping(); err != nil {
-		log.Fatal("Database not reachable:", err)
-	}
+    // --- Запуск Cron Jobs
+    cronjobs.StartCronJobs()
 
-	a := app.New(db, cfg)
-	mux := a.SetupRoutes()
+    // --- Настройка сервера
+    a := app.New(db, cfg)
+    mux := a.SetupRoutes()
 
-	fmt.Println("Uade API running on port:", cfg.Port)
-	log.Fatal(http.ListenAndServe(":"+cfg.Port, mux))
+    fmt.Println("Uade API running on port:", cfg.Port)
+    log.Fatal(http.ListenAndServe(":"+cfg.Port, mux))
 }
